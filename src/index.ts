01354ef641dd460b7a3083b8bb6b0fc764d32d2f
alfapsycho <psao69@gmail.com>
2020-09-15 09:18:26 +0200

    added cors package and set origin to allow frontend to connect with backend

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { MyContext } from './types';
+import cors from 'cors';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -23,6 +24,13 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
 
   app.use(
     session({
@@ -57,7 +65,12 @@ const main = async () => {
   });
 
   //Skapar en GraphQL endpoint
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: false,
+    },
+  });
 
   // Berättar vilken port express ska lyssna på
   app.listen(3000, () => {
